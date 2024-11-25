7ba0a16447e42eec87afa8d3013fa5e53c931cb6
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2024-11-25 11:51:49 +0700

    commit3

--- resources/views/daftar_pembelian.blade.php
+++ resources/views/daftar_pembelian.blade.php
@@ -1,10 +1,10 @@
 <x-layot>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <div class="container mx-auto px-4 py-8">
         <div class="card">
             <div class="card-header">
                 Data Pembelian
+                <button class="btn btn-danger gap-x-1.5 rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-800 hover:bg-gray-700" onclick="window.location.href='#'">Add</button>
             </div>
             <div class="card-body">
                 <table>
@@ -17,6 +17,8 @@
                             <th class="border border-gray-300 px-4 py-2 font-medium text-gray-900">Jumlah Barang</th>
                             <th class="border border-gray-300 px-4 py-2 font-medium text-gray-900">Diskon</th>
                             <th class="border border-gray-300 px-4 py-2 font-medium text-gray-900">Total Harga</th>
+                            <th class="border border-gray-300 px-4 py-2 font-medium text-gray-900"></th>
+                            <th class="border border-gray-300 px-4 py-2 font-medium text-gray-900"></th>
                         </tr>
                     </thead>
 
@@ -29,7 +31,17 @@
                             <td class="border border-gray-300 px-4 py-2 font-medium text-gray-500">{{ $dtbeli->KODEBRG}}</td>
                             <td class="border border-gray-300 px-4 py-2 font-medium text-gray-500">{{ $dtbeli->QTY}}</td>
                             <td class="border border-gray-300 px-4 py-2 font-medium text-gray-500">{{ $dtbeli->DISKON}}</td>
-                            <td class="border border-gray-300 px-4 py-2 font-medium text-gray-500">{{ $dtbeli->TOTALRP}}</td>    
+                            <td class="border border-gray-300 px-4 py-2 font-medium text-gray-500">{{ $dtbeli->TOTALRP}}</td>
+                            <td class="border border-gray-300 px-4 py-2 font-medium text-gray-500">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <button class="btn btn-danger gap-x-1.5 rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-800 hover:bg-gray-700" onclick="window.location.href='#'">Edit</button>
+                                </div>
+                            </td>
+                            <td class="border border-gray-300 px-4 py-2 font-medium text-gray-500">
+                                <div class="d-flex justify-content-between mb-3">
+                                    <button class="btn btn-danger gap-x-1.5 rounded-md bg-gray-400 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-800 hover:bg-gray-700" onclick="window.location.href='#'">Delete</button>
+                                </div>
+                            </td>
                         </tr>    
                     </tbody>
             @endforeach
